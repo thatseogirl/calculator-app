d1b88970e742d37fbcee153ac21cd33727c3ee65
Tobi <arotimi.tobiloba@gmail.com>
2022-03-09 12:51:09 +0100

    first commit

---
+++ src/component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header'>
+    <h5>calc</h5>
+    <div className='theme'>
+      <p>Theme</p>
+    </div>
+    </div>
+  )
+}
---
+++ src/component/Input/Input.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {FormControl} from "react-bootstrap";
+
+export default function Input() {
+  return (
+    <div>
+         <FormControl
+         className='input'
+         type="number"
+        //  onClick={handleChange}
+    />
+    </div>
+  )
+}
